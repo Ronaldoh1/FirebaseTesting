069640e6db85f77ba318a7181e2502acb3b21c9c
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-09-01 00:25:06 -0700

    able to create subpaths in db

--- FirebaseTesting/ViewController.swift
+++ FirebaseTesting/ViewController.swift
@@ -10,10 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+
+    var ref = Firebase (url: "https://testtttappp.firebaseio.com/")
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+
+        //listens to whatever data has been changed.
+
+        let newObject = "this is a new object"
+
+        ref.childByAppendingPath("ObjectPath").setValue(newObject)
+
+        ref.childByAppendingPath("ObjectPathTwo").childByAppendingPath("SubPath").setValue("This is a further path")
+
+
+        ref.observeEventType(.Value, withBlock: {
+            snapshot in
+
+            println(snapshot.value.objectForKey("ObjectPath") as! String)
+
+        })
+
+
+
+
+           }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
